bca5f40ce2ea17e88fd4f0e314fba0069720f28d
longlouis1222 <67300066+longlouis1222@users.noreply.github.com>
2022-01-18 11:45:11 +0700

    first commit

---
+++ src/components/Navigation.vue
@@ -0,0 +1,41 @@
+<template>
+  <header class="bg-at-light-green text-white">
+    <nav class="container py-5 px-4 flex-col gap-4 items-center sm:flex-row">
+      <div class="flex items-center gap-x-4">
+        <img class="w-14" src="../assets/images/dumbbell-light.png" alt="" />
+        <h1 class="text-lg">Active Tracker</h1>
+        <ul class="flex flex-1 justify-end gap-x-10">
+          <router-link class="cursor-pointer" :to="{ name: 'Home' }"
+            >Home</router-link
+          >
+          <router-link class="cursor-pointer" :to="{ name: '' }"
+            >Create</router-link
+          >
+          <router-link class="cursor-pointer" :to="{ name: 'Login' }"
+            >Login</router-link
+          >
+          <li @click="logout" class="cursor-pointer">Logout</li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+import { supabase } from "../supabase/init";
+import { useRouter } from "vue-router";
+export default {
+  setup() {
+    // Get user from store
+
+    // Setup ref to router
+    const router = useRouter();
+    // Logout function
+    const logout = async () => {
+      await supabase.auth.signout();
+      router.push({ name: "Home" });
+    };
+    return { logout };
+  },
+};
+</script>
